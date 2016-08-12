f01ecd92a9449aa5332437539232fe6ad5144eed
SNikon <wrAtrA9u12>
2016-08-12 01:48:08 +0000

    Still in QuetzyPortable, karma tests moving forward. Game seems to have everything needed to be closed.

--- js/game.js
+++ js/game.js
@@ -5,12 +5,75 @@
 var Game = (function() {
     "use strict";
 
-    // Public static functions
-    function _makePrivateState(rootBoard, index, size) {
+    // Private static functions
+
+    function _makeNeighbourIndexArray(index, size) {
+        var row = ~~(index / size);
+        var col = index % size;
+
+        var prevRow = row > 0 ? row - 1 : size - 1;
+        var nextRow = row < (size - 1) ? row + 1 : 0;
+        var prevCol = col > 0 ? col - 1 : size - 1;
+        var nextCol = col < (size - 1) ? col + 1 : 0;
+
+        return  [   [prevRow, prevCol],
+            [prevRow, col],
+            [prevRow, nextCol],
+            [row, prevCol],
+            [row, nextCol],
+            [nextRow, prevCol],
+            [nextRow, col],
+            [nextRow, nextCol]
+        ].map(function(pos) {
+            return pos[0] * size + pos[1]
+        });
+    }
+
+    function _makeLivingNeighbours(board, index, size) {
+        return _makeNeighbourIndexArray(index, size).map(function(idx) {
+            return board[idx];
+        }).filter(function(territory) {
+            return territory.state === constructor.ALIVE;
+        });
+    }
+
+    function _makeTerritoryFromPublic(rootBoard, index, size) {
         return {
-            'state': rootBoard[index], // Public state is a boolean
-            'neighbours': []
+            state: rootBoard[index], // Public state is a boolean
+            row: ~~(index / size),
+            column: (index % size),
+            neighbours: []
         };
+    }
+
+    function _copyBoard(board) {
+        return board.map(function(territory) {
+            return {
+                row: territory.row,
+                column: territory.column,
+                state: territory.state,
+                neighbours: territory.neighbours.slice()
+            }
+        });
+    }
+
+    function _makeDefaultRules() {
+        return [
+            {
+                // Any live territory with less than 2 live neighbours dies from underpopulation,
+                // Any live territory with more than 3 live neighbours dies from overpopulation
+                on: constructor.ALIVE,
+                changeCondition: function (territory) {
+                    return territory.neighbours.length < 2 || territory.neighbours.length > 3;
+                }
+            }, {
+                // Any dead territory with exactly 3 live neighbours becomes alive
+                on: constructor.DEAD,
+                changeCondition: function (territory) {
+                    return territory.neighbours.length === 3;
+                }
+            }
+        ];
     }
 
     // Public static value properties
@@ -28,10 +91,13 @@ var Game = (function() {
     });
 
     function constructor() {
+        var initialBoard = null;
         var board = null;
-        var nextBoard = null;
         var size = 0;
-        var live = [];
+        var rules = [];
+        var deadRules = [];
+        var toProcess = [];
+        var end = true;
 
         this.init = function(rootBoard) {
             // If already initialized, don't do it again. Make a new game if you want.
@@ -44,7 +110,6 @@ var Game = (function() {
             // Valid board is length is positive and if the square root of the length is an integer (no decimals)
             var isSquare = rootBoard.length > 0 && (~~sqrt) === sqrt;
             if(isSquare) {
-                size = sqrt;
 
                 var valid = true;
                 board = rootBoard.map(function(publicState, index) {
@@ -54,7 +119,7 @@ var Game = (function() {
                         return null;
                     }
 
-                    return _makePrivateState(rootBoard, index, size);
+                    return _makeTerritoryFromPublic(rootBoard, index, sqrt);
                 });
 
                 if(!valid) {
@@ -62,11 +127,26 @@ var Game = (function() {
                     throw new Error('Invalid state in board');
                 }
 
-                nextBoard = board.map(function (x) {
-                    return x;
-                }); // Just copy
-                live = board.map(function (x) {
-                    return {};
+                initialBoard = _copyBoard(board); // Keep a copy
+                size = sqrt;
+                end = false;
+
+                board.forEach(function(territory, index) {
+                    territory.neighbours = _makeLivingNeighbours(board, index, size);
+                });
+
+                rules = _makeDefaultRules();
+                deadRules = rules.filter(function(rule) {
+                    return rule.on === constructor.DEAD;
+                });
+
+                toProcess = board.filter(function(territory) {
+                    // Currently alive
+                    return (territory.state === constructor.ALIVE) ||
+                        // Or dead and about to change next stage
+                        deadRules.some(function(rule) {
+                            return rule.changeCondition(territory);
+                        })
                 });
             } else {
                 throw new Error('Board must be square')
@@ -74,33 +154,86 @@ var Game = (function() {
         };
 
         this.state = function(row, column) {
-            if((row == null && column == null) || (row != null && column != null)) {
-                if (row == null) // No slot requested, return the board
-                    return board.map(function (x) {
-                        return x.state;
-                    }); // Only information about being dead or alive, internal states are hidden
-                else {
-                    // Valid parameters
-                    if(row <= size && column <= size && row > 0 && column > 0) {
-                        return board[row * size + column]
-                    }
+            if(board != null) {
+                if ((row == null && column == null) || (row != null && column != null)) {
+                    if (row == null) // No slot requested, return the board
+                        return board.map(function (territory) {
+                            return territory.state;
+                        }); // Only information about being dead or alive, internal states are hidden
                     else {
-                        throw new Error('Invalid arguments');
+                        // Valid parameters
+                        if (row <= size && column <= size && row > 0 && column > 0)
+                            return board[row * size + column].state;
+                        else
+                            throw new Error('Invalid arguments');
                     }
                 }
             }
+            throw new Error('Game not initialized');
+
+        };
+
+        this.tick = function() {
+            if(!end) {
+                // A (deep) copy of the board is made so that new states don't affect the current tick
+                var newBoard = _copyBoard(board);
+                var newToProcess = [];
+                // For each territory check the conditions to change state in the PREVIOUS board and update in the NEW board
+                toProcess.forEach(function (territory) {
+                    var changed = rules.some(function (rule) {
+                        return rule.on === territory.state && rule.changeCondition(territory);
+                    });
+                    var currentTerritoryIndex = territory.row * size + territory.column;
+                    if (changed) {
+                        newBoard[currentTerritoryIndex].state = (territory.state === constructor.ALIVE ? constructor.DEAD : constructor.ALIVE);
+                        _makeNeighbourIndexArray(currentTerritoryIndex, size).forEach(function (neighbourIndex) {
+                            newBoard[neighbourIndex].neighbours.push(newBoard[currentTerritoryIndex]);
+                            var livingCondition = deadRules.some(function(rule) {
+                                return rule.changeCondition(newBoard[neighbourIndex])
+                            });
+                            if(livingCondition)
+                                toProcess.push(newBoard[currentTerritoryIndex]);
+                        });
+                    }
+                    if(!changed && territory.state === constructor.ALIVE || changed && territory.state === constructor.DEAD)
+                        newToProcess.push(newBoard[currentTerritoryIndex]);
+                });
+                toProcess = newToProcess;
+                var anyChange = false;
+                var len = board.length;
+                for (var i = 0; !anyChange && i < len; i++)
+                    anyChange = board[i].state !== newBoard[i].state;
+
+                if (anyChange)
+                    board = newBoard;
+                else
+                    end = true;
+            }
+        };
+
+        this.reset = function() {
+            if(initialBoard == null)
+                throw new Error('Game not initialized');
+
+            board = _copyBoard(initialBoard);
+            toProcess = board.filter(function(territory) {
+                return territory.state === constructor.ALIVE;
+            });
+            end = false;
+        };
+
+        this.gameover = function() {
+            return end;
         };
 
         this.boardSize = function() {
-            if(board != null)
-                return Math.sqrt(board.length);
-            return 0;
+            return size;
         };
     }
 
     // Statics
     constructor.makeDefaultPublicState = function() {
-        return Game.ALIVE;
+        return Game.DEAD;
     };
     constructor.makeDefaultPublicBoard = function(size) {
         if(size == null || (size != (~~size)) || size <= 0)
--- specs/game-tests.js
+++ specs/game-tests.js
@@ -125,25 +125,228 @@
             });
 
             it('should fail if initialized more than once', function() {
-                expect(false).toBe(true);
+                var that = this;
+
+                expect(function() {
+                    that.game.init(that.testBoard1)
+                }).not.toThrow();
+                expect(function() {
+                    that.game.init(that.testBoard1)
+                }).toThrow();
             });
 
-            it('should only return public state', function() {
-                expect(false).toBe(true);
+            it('should only display public state', function() {
+                var that = this;
+
+                expect(function() {
+                    that.game.init(that.testBoard2)
+                }).not.toThrow();
+                var newBoard = this.game.state();
+                expect(newBoard).not.toBe(this.testBoard2);
+                expect(newBoard).toEqual(this.testBoard2);
             })
 
         });
 
         describe('ticker', function() {
-            beforeAll(function() {
+            beforeEach(function() {
                 this.game = new Game();
             });
 
-            afterAll(function() {
+            afterEach(function() {
                 this.game = null;
             });
 
+            it('should advance the state once per tick', function() {
+                var that = this;
+                // var initialState = [
+                //     false,  true,   false,  false,  false,
+                //     false,  true,   false,  false,  false,
+                //     false,  false,  false,  false,  false,
+                //     false,  false,  false,  false,  false,
+                //     false,  true,  false,  false,  false
+                // ];
+                var initialState = [
+                    false,  true,   true,
+                    false,  true,   false,
+                    false,  false,  false
+                ];
+                var expectedState1 = [
+                    true,   true,   true,
+                    true,   true,   true,
+                    true,   true,   true
+                ];
+                var expectedState2 = [
+                    false,  false,  false,
+                    false,  false,  false,
+                    false,  false,  false
+                ];
+                this.game.init(initialState);
 
+                expect(this.game.state()).toEqual(initialState);
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+                expect(this.game.state()).toEqual(expectedState1);
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+                expect(this.game.state()).toEqual(expectedState2);
+            });
+
+            it('should end the game if reached a deadlock with no changes', function() {
+                var that = this;
+
+                var initialState = [
+                    false,  true,   true,
+                    false,  true,   false,
+                    false,  false,  false
+                ];
+                var expectedState1 = [
+                    true,   true,   true,
+                    true,   true,   true,
+                    true,   true,   true
+                ];
+                var deadlockState1 = [
+                    false,  false,  false,
+                    false,  false,  false,
+                    false,  false,  false
+                ];
+                var deadlockState2 = [
+                    false,  false,  false,
+                    false,  false,  false,
+                    false,  false,  false
+                ];
+
+                expect(this.game.gameover()).toBeTruthy();
+
+                expect(function() {
+                    that.game.init(initialState);
+                }).not.toThrow();
+
+                expect(this.game.gameover()).toBeFalsy();
+
+                expect(this.game.state()).toEqual(initialState);
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+                expect(this.game.state()).toEqual(expectedState1);
+
+                expect(this.game.gameover()).toBeFalsy();
+
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+                expect(this.game.state()).toEqual(deadlockState1);
+
+                expect(this.game.gameover()).toBeFalsy();
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+                expect(this.game.state()).toEqual(deadlockState2);
+
+                expect(this.game.gameover()).toBeTruthy();
+            });
+
+            it('should have no effect once the game is over', function() {
+                var that = this;
+
+                var deadlockState = [
+                    false,  false,  false,
+                    false,  false,  false,
+                    false,  false,  false
+                ];
+
+                expect(this.game.gameover()).toBeTruthy();
+
+                expect(function() {
+                    that.game.init(deadlockState);
+                }).not.toThrow();
+
+                expect(this.game.gameover()).toBeFalsy();
+                expect(this.game.state()).toEqual(deadlockState);
+
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+
+                expect(this.game.state()).toEqual(deadlockState);
+                expect(this.game.gameover()).toBeTruthy();
+
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+
+                expect(this.game.state()).toEqual(deadlockState);
+                expect(this.game.gameover()).toBeTruthy();
+
+                expect(function() {
+                    that.game.tick()
+                }).not.toThrow();
+
+                expect(this.game.state()).toEqual(deadlockState);
+                expect(this.game.gameover()).toBeTruthy();
+            });
+        });
+
+        describe('reset', function() {
+            beforeEach(function () {
+                this.game = new Game();
+            });
+
+            afterEach(function () {
+                this.game = null;
+            });
+
+            it('should throw if the game is not initialized', function() {
+                var that = this;
+                expect(function() {
+                    that.game.reset()
+                }).toThrow(new Error('Game not initialized'));
+            });
+
+            it('should set the initial state', function () {
+                var that = this;
+
+                var initialState = [
+                    false, true, true,
+                    false, true, false,
+                    false, false, false
+                ];
+                var deadlockState = [
+                    false, false, false,
+                    false, false, false,
+                    false, false, false
+                ];
+
+
+                expect(function () {
+                    that.game.init(initialState);
+                }).not.toThrow();
+
+                expect(function () {
+                    that.game.tick();
+                    that.game.tick();
+                    that.game.tick()
+                }).not.toThrow();
+
+                expect(this.game.state()).toEqual(deadlockState);
+                expect(this.game.gameover()).toBeTruthy();
+
+                this.game.reset();
+                expect(this.game.gameover()).toBeFalsy();
+                expect(this.game.state()).toEqual(initialState);
+
+
+                expect(function () {
+                    that.game.tick();
+                    that.game.tick();
+                    that.game.tick()
+                }).not.toThrow();
+
+                expect(this.game.state()).toEqual(deadlockState);
+                expect(this.game.gameover()).toBeTruthy();
+            });
         })
     });
 })();
